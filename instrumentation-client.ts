0859c462ff3c9f161a5eb2d198137312c15d28f4
Sai Bhaskar Devatha <saibhaskardevatha@gmail.com>
2025-06-24 15:28:28 +0530

    Fixed Build Error

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,12 +1,20 @@
-import posthog from "posthog-js"
+import type { PostHog } from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  capture_pageview: 'history_change',
-  capture_pageleave: true, // Enable pageleave capture
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
-});
+let posthog: PostHog | { capture: (...args: unknown[]) => void } = { capture: () => {} };
+
+if (typeof window !== "undefined") {
+  // Only import and initialize posthog-js on the client
+  import("posthog-js").then((posthogJs) => {
+    posthogJs.default.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      capture_pageview: 'history_change',
+      capture_pageleave: true, // Enable pageleave capture
+      capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+      debug: process.env.NODE_ENV === "development",
+    });
+    posthog = posthogJs.default;
+  });
+}
 
 export default posthog;
